f6271819b87f794cb19d8a17b85a0311d957e236
2018008922 <2018008922@hanyang.ac.kr>
2023-11-23 20:51:02 +0900

    first commit

---
+++ spring/src/main/java/umc/study/domain/mapping/MemberMission.java
@@ -0,0 +1,22 @@
+package umc.study.domain.mapping;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+import umc.study.domain.enums.MissionStatus;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
