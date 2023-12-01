d219050312fa5c3dfb423f12cbbc08a6e2c58161
2018008922 <2018008922@hanyang.ac.kr>
2023-12-01 14:01:40 +0900

    Mission8

---
+++ spring/src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,26 @@
+package umc.study.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+}
