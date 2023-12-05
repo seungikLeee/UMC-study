1d3c49e88f05e0d0b6d1e920ed94a78f74eaef9e
2018008922 <2018008922@hanyang.ac.kr>
2023-12-05 19:01:54 +0900

    setting

--- spring/src/main/java/umc/study/apiPayload/code/BaseCode.java
+++
@@ -1,7 +0,0 @@
-package umc.study.apiPayload.code;
-
-public interface BaseCode {
-    public ReasonDTO getReason();
-
-    public ReasonDTO getReasonHttpStatus();
-}
--- spring/src/main/java/umc/study/domain/enums/MissionStatus.java
+++
@@ -1,5 +0,0 @@
-package umc.study.domain.enums;
-
-public enum MissionStatus {
-    CHALLENGING, COMPLETE
-}
